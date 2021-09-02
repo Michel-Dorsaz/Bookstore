eb13c2492ad4723368a64ab840acd30524c9d200
MDorsaz <michel.dorsaz@students.hevs.ch>
2021-09-02 16:39:27 +0300

    1st commit

---
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,41 @@
+package com.example.Bookstore.domain;
+
+public class Book {
+
+	private String title, author, isbn;
+	private short year;
+	private float price;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public short getYear() {
+		return year;
+	}
+	public void setYear(short year) {
+		this.year = year;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+}
